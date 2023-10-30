8deaf2ba352ff69f1c5a2c92f00e894d5738f68f
Quyet Thang <quyetthangmarvel@gmail.com>
2023-10-30 19:09:02 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,3 +15,20 @@ export const signup = async (req, res, next) => {
         next(error)
     }
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email })
+        if (!validUser) return next(errorHandler(404, 'User not found!'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        //destructuring assignment with password, _doc means document data of user
+        const { password: pass, ...rest } = validUser._doc;
+        //rest contains user data except password
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
